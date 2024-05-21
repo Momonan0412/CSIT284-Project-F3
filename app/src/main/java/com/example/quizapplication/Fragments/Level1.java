729da75a19adf9b49415d2027b1404f265beb2ee
meepmeep <elijahsabay@gmail.com>
2024-05-22 05:16:36 +0800

    works but weird stuff is going on..me fix later

---
+++ app/src/main/java/com/example/quizapplication/Fragments/Level1.java
@@ -0,0 +1,175 @@
+package com.example.quizapplication.Fragments;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.example.quizapplication.R;
+import com.example.quizapplication.activities.Study;
+import com.example.quizapplication.utils.DatabaseUtilities;
+import com.google.firebase.annotations.concurrent.Background;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Level1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Level1 extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    LinearLayout layout;
+
+    TextView kanji, furina, english,mnemonic;
+//    LinearLayout displayStuff;
+    ScrollView scroll,displayStuff;
+    JSONArray jsonArray;
+
+    public Level1() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Level1.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Level1 newInstance(String param1, String param2) {
+        Level1 fragment = new Level1();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_level1, container, false);
+    }
+
+    //Reference ======== https://stackoverflow.com/questions/6495898/findviewbyid-in-fragment =========
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        layout = getView().findViewById(R.id.orient);
+        english = getView().findViewById(R.id.fgEnglish);
+        furina = getView().findViewById(R.id.fgTxtReading);
+        kanji = getView().findViewById(R.id.frgTxtKanji);
+        mnemonic = getView().findViewById(R.id.fgMnemonic);
+        scroll = getView().findViewById(R.id.fgScrollView);
+        displayStuff = getView().findViewById(R.id.fgDisplayScrollView);
+//        displayStuff = getView().findViewById(R.id.llDisplayStudy);
+
+        displayStuff.setClickable(false);
+        displayStuff.setVisibility(View.INVISIBLE);
+
+//        displayStuff.setBackground();
+//        displayStuff.setPadding(2,2,2,2);
+//        displayStuff.setBackgroundColor(Color.GRAY);
+
+
+
+        System.out.println(layout.isActivated());
+        DatabaseUtilities.getKanji("Level "+ Study.getLevel(),getContext(),(data)->{
+            this.jsonArray = data;
+            System.out.println(this.jsonArray.length()+"HENFDJFDLFNDSFLEN");
+            Button[] btnArray = new Button[jsonArray.length()];
+            try{
+                for(int i=0; i<jsonArray.length();i++){
+                    Button btn = new Button(getContext());
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    String kanjiSymbol = jsonObject.getString("kanji");
+                    btn.setText(kanjiSymbol);
+                    btn.setTextSize(30);
+                    btn.setPadding(10, 10, 10, 10);
+                    btn.setId(i);
+                    btnArray[i] = btn;
+
+
+                    layout.addView(btn);
+
+                }
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+
+            for(int i = 0; i< jsonArray.length();i++){
+                int idkWtfImStillDoing = i;
+                btnArray[i].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            JSONObject jsonObject = jsonArray.getJSONObject(idkWtfImStillDoing);
+
+                            String kanjiSymbol = jsonObject.getString("kanji");
+                            String furigana = jsonObject.getString("furigana");
+                            String englishMeaning = jsonObject.getString("english");
+                            String mnemonic1 = jsonObject.getString("mnemonic");
+                            System.out.println(mnemonic1+" Button "+btnArray[idkWtfImStillDoing].getId()+"is pressed");
+
+                            displayStuff.setClickable(true);
+                            displayStuff.setVisibility(View.VISIBLE);
+                            scroll.setVisibility(View.INVISIBLE);
+
+                            kanji.setTextSize(50);
+                            kanji.setText(kanjiSymbol);
+                            english.setText(englishMeaning);
+                            furina.setText(furigana);
+                            mnemonic.setText(mnemonic1);
+
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+
+    }
+
+}
--- app/src/main/java/com/example/quizapplication/activities/Study.java
+++ app/src/main/java/com/example/quizapplication/activities/Study.java
@@ -1,13 +1,19 @@
 package com.example.quizapplication.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
 
+import com.example.quizapplication.Fragments.Level1;
 import com.example.quizapplication.R;
 import com.example.quizapplication.utils.DatabaseUtilities;
 
@@ -15,28 +21,180 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class Study extends AppCompatActivity {
+    Button btn1, btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn10,btnExit;
+    LinearLayout display;
+    FragmentContainerView view;
+    private static int level;
+
+    public static int getLevel() {
+        return level;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study);
 
-        // TODO: IMPLEMENT STUFF
-        // @ Elijah Reference
-        DatabaseUtilities.getKanji("Level 1", getApplicationContext()  ,(o) -> {
-            JSONArray jsonArray = o;
-            for(int i = 0; i < jsonArray.length(); i++){
-                try {
-                    System.out.println(jsonArray.getJSONObject(i).getString("mnemonic"));
-                    System.out.println(jsonArray.getJSONObject(i).getString("kanji"));
-                    System.out.println(jsonArray.getJSONObject(i).getString("furigana"));
-                    System.out.println(jsonArray.getJSONObject(i).getString("english"));
-                    // @ DatabaseUtilities line 101, 102, 103 and 104
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+
+//=======
+        btn1 = findViewById(R.id.btnStudy1);
+        btn2 = findViewById(R.id.btnStudy2);
+        btn3 = findViewById(R.id.btnStudy3);
+        btn4 = findViewById(R.id.btnStudy4);
+        btn5 = findViewById(R.id.btnStudy5);
+        btn6 = findViewById(R.id.btnStudy6);
+        btn7 = findViewById(R.id.btnStudy7);
+        btn8 = findViewById(R.id.btnStudy8);
+        btn9 = findViewById(R.id.btnStudy9);
+        btn10 = findViewById(R.id.btnStudy10);
+
+        view = findViewById(R.id.fgDisplay);
+        display = findViewById(R.id.vlDisplay);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                level = 1;
+                System.out.println("this is clicked");
+                FragmentManager manage = getSupportFragmentManager();
+                manage.beginTransaction()
+                        .replace(R.id.fgDisplay,Level1.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .commit();
+
             }
         });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                level = 2;
+                System.out.println("this is clicked");
+                FragmentManager manage = getSupportFragmentManager();
+                manage.beginTransaction()
+                        .replace(R.id.fgDisplay,Level1.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                level = 3;
+                System.out.println("this is clicked");
+                FragmentManager manage = getSupportFragmentManager();
+                manage.beginTransaction()
+                        .replace(R.id.fgDisplay,Level1.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                level = 4;
+                System.out.println("this is clicked");
+                FragmentManager manage = getSupportFragmentManager();
+                manage.beginTransaction()
+                        .replace(R.id.fgDisplay,Level1.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                level = 5;
+                System.out.println("this is clicked");
+                FragmentManager manage = getSupportFragmentManager();
+                manage.beginTransaction()
+                        .replace(R.id.fgDisplay,Level1.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                level = 6;
+                System.out.println("this is clicked");
+                FragmentManager manage = getSupportFragmentManager();
+                manage.beginTransaction()
+                        .replace(R.id.fgDisplay,Level1.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                level = 7;
+                System.out.println("this is clicked");
+                FragmentManager manage = getSupportFragmentManager();
+                manage.beginTransaction()
+                        .replace(R.id.fgDisplay,Level1.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                level = 8;
+                System.out.println("this is clicked");
+                FragmentManager manage = getSupportFragmentManager();
+                manage.beginTransaction()
+                        .replace(R.id.fgDisplay,Level1.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                level = 9;
+                System.out.println("this is clicked");
+                FragmentManager manage = getSupportFragmentManager();
+                manage.beginTransaction()
+                        .replace(R.id.fgDisplay,Level1.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+        btn10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                level = 10;
+                System.out.println("this is clicked");
+                FragmentManager manage = getSupportFragmentManager();
+                manage.beginTransaction()
+                        .replace(R.id.fgDisplay,Level1.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+        // TODO: Implement this activity like flashcard ish
+//>>>>>>> Stashed changes
 
     }
+
+
 }
